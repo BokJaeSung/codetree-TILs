fe1c7ae1dcbe53181be8873f7f45ade9bee4767a
BokJaeSung <87299396+BokJaeSung@users.noreply.github.com>
2024-07-26 14:17:56 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 30ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.cpp
+++ 240726/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 int n, k;
 int ans = INT_MAX;
@@ -16,7 +17,7 @@ bool solve(int mid)
         return false;
     }
 }
-int main()
+signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
