7decf7e5762e393dddbaf045cb5713ebbd38620c
BokJaeSung <87299396+BokJaeSung@users.noreply.github.com>
2024-07-28 19:47:54 +0900

    Title: 금 채굴하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240728/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int board[21][21];
+int n, m, ans;
+int solve(int x, int y, int k)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (abs(x - i) + abs(y - j) <= k)
+                cnt += board[i][j];
+    return cnt;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> board[i][j];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = 0; k <= 2 * (n - 1); k++){
+                int num_of_gold=solve(i,j,k);
+            }
+                if (num_of_gold * m >= k * k + (k + 1) * (k + 1))
+                    ans = max(ans,num_of_gold);
+    cout << ans;
+    return 0;
+}
