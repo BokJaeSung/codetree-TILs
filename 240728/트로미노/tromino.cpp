cb59839e0a8c3b272f1f623ce0efaf22476e3a00
BokJaeSung <87299396+BokJaeSung@users.noreply.github.com>
2024-07-28 20:23:40 +0900

    Title: 트로미노, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240728/트로미노/tromino.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+int ans, n, m;
+int board[201][201];
+
+int solve(int x, int y, int dir)
+{
+    int cnt = 0;
+    switch (dir)
+    {
+    case 0:
+        if (In_ragne(x, y + 1) && In_ragne(x - 1, y))
+            cnt += board[x][y + 1] + board[x][y] + board[x - 1][y];
+        else
+        {
+            return 0;
+        }
+        break;
+    case 1:
+        if (In_ragne(x, y - 1) && In_ragne(x + 1, y))
+            cnt += board[x][y - 1] + board[x][y] + board[x + 1][y];
+        else
+        {
+            return 0;
+        }
+        break;
+    case 2:
+        if (In_ragne(x, y + 1) && In_ragne(x + 1, y))
+            cnt += board[x][y + 1] + board[x][y] + board[x + 1][y];
+        else
+        {
+            return 0;
+        }
+        break;
+    case 3:
+        if (In_ragne(x, y - 1) && In_ragne(x - 1, y))
+            cnt += board[x][y - 1] + board[x][y] + board[x - 1][y];
+        else
+        {
+            return 0;
+        }
+        break;
+    case 4:
+        if (In_ragne(x + 1, y) && In_ragne(x - 1, y))
+            cnt += board[x + 1][y] + board[x][y] + board[x - 1][y];
+        else
+        {
+            return 0;
+        }
+        break;
+    case 5:
+        if (In_ragne(x, y + 1) && In_ragne(x , y-1)
+            cnt += board[x][y + 1] + board[x][y] + board[x][y-1];
+        else
+        {
+            return 0;
+        }
+        break;
+    default:
+        break;
+    }
+    return cnt;
+}
+bool In_ragne(int x, int y)
+{
+    return x >= 0 && x < n && y >= 0 && y < n;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> board[i][j];
+    for (int i = 0; j < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int dir = 0; dir < 6; dir++)
+            {
+                ans = max(ans, solve(i, j, dir));
+            }
+    return 0;
+}
